72f8715dfc0cefab084800af048b400c4b38c10d
Antoine Arnoud <antoine.arnoud@yale.edu>
2021-01-09 17:02:18 +0900

    add recording of files for stats

--- converter.py
+++ converter.py
@@ -10,8 +10,11 @@ import base64
 import datetime
 
 st.title("Stata Converter WebApp")
-st.info("Application created by antoine.arnoud@pm.com to convert Stata files into previous versions.")
-st.markdown('Desktop application available for <a href="https://github.com/antoinearnoud/stata_converter"> Mac </a>', unsafe_allow_html=True)
+st.info("Application by antoine.arnoud@gmail.com to convert Stata files into oldest Stata versions.")
+#st.markdown('Desktop application available for <a href="https://github.com/antoinearnoud/stata_converter"> Mac </a>', unsafe_allow_html=True)
+
+#st.sidebar.info('Created by antoine.arnoud@gmail.com')
+#st.sidebar.warning('Report errors')
 
 @st.cache(suppress_st_warning=True, show_spinner=False)
 def load_stata(filename):
@@ -38,9 +41,21 @@ def delete_old_files():
         curpath = os.path.join("temp", file)
         file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
         #st.write(file + " was modified at " + str(file_modified))
-        if datetime.datetime.now() - file_modified > datetime.timedelta(hours=1):
+        if datetime.datetime.now() - file_modified > datetime.timedelta(minutes = 20): #(hours=1):
             print("deleting: " + file)
             os.remove(curpath)
+
+def record_file_name(filename, size, version):
+    now = datetime.datetime.now().isoformat()
+    filepath = 'filenames.csv'
+    if os.path.exists(filepath):
+        openmode = 'a'
+    else:
+        openmode = 'w'
+    with open(filepath, mode = openmode) as recordfile:
+        recordfile.write(filename +',' + str(size) + ',' + version + ',' + now)
+        recordfile.write('\n')
+
 
 # delete files in /temp/ older than 1 hour
 delete_old_files()
@@ -50,6 +65,7 @@ if not (file_uploaded is None):
     df = load_stata(file_uploaded)
     file_details = {"FileName":file_uploaded.name,"FileType":file_uploaded.type,"FileSize":file_uploaded.size}
     filename = file_details["FileName"]
+    size = file_details['FileSize']
     version = st.radio("Convert file to Stata version", ["Stata 10", "Stata 13", "Stata 14"])
     base_filename = filename.replace(".dta","")
     if version == "Stata 10": extension, ver = "_v10", 114
@@ -57,6 +73,7 @@ if not (file_uploaded is None):
     if version == "Stata 14": extension, ver = "_v14", 118
     newname = base_filename + extension + ".dta"
     if st.button('Convert file'):
+        record_file_name(filename, size, version)
         with st.spinner('Conversion in progress...'):
             df.to_stata(os.path.join('temp', newname), version = ver, write_index = False)
         #st.markdown(get_table_download_link(df), unsafe_allow_html=True)
